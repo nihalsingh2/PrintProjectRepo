d2b23df1ddd10040c5081866e7bea556b0fcfcef
Nihal Singh <nihalsingh1802@gmail.com>
2024-04-10 16:04:00 +0530

    commitingg two files

---
+++ src/myPackage/Just.java
@@ -0,0 +1,10 @@
+package myPackage;
+
+public class Just {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("just kya chal raha");
+	}
+
+}
---
+++ src/myPackage/hello.java
@@ -0,0 +1,10 @@
+package myPackage;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
